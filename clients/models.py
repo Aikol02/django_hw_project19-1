4fca7fc09e7479a66e43ecab9e4363a2ba865ac0
Aikol <urmatbekovaaiziya@gmail.com>
2022-08-23 17:52:48 +0600

    hw3

--- clients/models.py
+++ clients/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 class Client(models.Model):
 	name = models.CharField(max_length=255)
-	address = models.CharField(max_length=255)
-	active = models.BooleanField(default=False)
-	bottles_ordered = models.IntegerField(default=20)
+	address = models.CharField(max_length=255, null=True, blank=True)
+	active = models.BooleanField(default=True, verbose_name="Является активным покупателем!")
+	bottles_ordered = models.IntegerField(default=1)
+	photo = models.ImageField(
+		verbose_name="Фото",
+		upload_to='photos',
+		null=True,
+		blank=True
+	)
--- clients/views.py
+++ clients/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from .models import Client
 
 
+def contacts(request):
+	return render(request, "contacts.html")
+
+
+def info(request):
+	return render(request, "info.html")
+
+
 def clients_list(request):
 	context = {}
 	client_list = Client.objects.all()
 	context["client_list"] = client_list
-	html_page = render(request, 'name.html', context)
-	return html_page
+	return render(request, 'clients.html', context)
