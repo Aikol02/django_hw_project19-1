f6550b5de10894e0abc6fe5200aa5dda8eb7a81e
Aikol <urmatbekovaaiziya@gmail.com>
2022-08-26 17:14:11 +0600

    HW4

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Client
+# from .models import Client
+from .models import Client, Order
 
 admin.site.register(Client)
+
+
+class OrderAdmin(admin.ModelAdmin):
+	model = Order
+	list_display = ['client', 'created_at', 'finished']
+	list_editable = ['finished']
+	fields = ['client', 'created_at', 'updated_at', 'description', 'finished']
+	readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Order, OrderAdmin)
--- clients/models.py
+++ clients/models.py
@@ -12,3 +12,32 @@ class Client(models.Model):
 		null=True,
 		blank=True
 	)
+
+	def __str__(self):
+		return f'{self.name}'
+
+
+class Order(models.Model):
+	client = models.ForeignKey(
+		to=Client,
+		null=True,
+		blank=True,
+		on_delete=models.SET_NULL,
+		related_name='order'
+	)
+
+	created_at = models.DateTimeField(
+		verbose_name="Date and time of creation of order",
+		auto_now_add=True,
+	)
+	updated_at = models.DateTimeField(
+		verbose_name="Date and time of update of order",
+		auto_now=True,
+	)
+	description = models.TextField(null=True, blank=True)
+	name = models.CharField(max_length=255)
+	contacts = models.CharField(null=True, blank=True, max_length=255)
+	finished = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f'{self.name} - {self.contacts}'
